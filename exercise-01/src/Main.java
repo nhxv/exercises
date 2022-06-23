82f98a0dc17089f51a6e9b08beafff56f24dc937
nhxv <ngohoangxuanvinh1995@gmail.com>
2022-06-23 12:08:35 +0700

    change method's name

--- exercise-01/src/Main.java
+++ exercise-01/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         int[] arr = {5, 9, 6, 8, 4, 6};
-        System.out.println(recursiveSum(arr, arr.length));
+        System.out.println(calculateSum(arr, arr.length));
     }
-    public static int recursiveSum(int[] arr, int size) {
+    public static int calculateSum(int[] arr, int size) {
         if (size < 1) return 0;
-        return recursiveSum(arr, size - 1) + arr[size - 1];
+        return calculateSum(arr, size - 1) + arr[size - 1];
     }
 }
